d99e3a06a76b3f7f2259b076b63357920a3f54a7
Ethan-C11 <ethan.collignon.1102@gmail.com>
2026-07-15 18:43:41 +0200

    feat: changed params

--- src/application/session/CreateSessionUseCase.ts
+++ src/application/session/CreateSessionUseCase.ts
@@ -1,7 +1,7 @@
 import type { Session } from "../../infrastructure/db/entities/session.entity.js";
 
 export class CreateSessionUseCase {
-  async execute(ownerId: number, event: string): Promise<Session> {
+  async execute(ownerId: number, sessionName: string): Promise<Session> {
     throw new Error("Not implemented");
   }
 }
