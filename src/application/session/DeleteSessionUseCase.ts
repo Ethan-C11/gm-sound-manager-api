6d3caa3b2a67fa6af0a35e3609563234a31df1a0
Ethan-C11 <ethan.collignon.1102@gmail.com>
2026-07-16 13:52:06 +0200

    feat: DeleteSessionUseCase

--- src/application/session/DeleteSessionUseCase.ts
+++ src/application/session/DeleteSessionUseCase.ts
@@ -1,5 +1,35 @@
+import {DeleteResult, Repository} from "typeorm";
+import {User} from "../../infrastructure/db/entities/user.entity.js";
+import {Session} from "../../infrastructure/db/entities/session.entity.js";
+import {AppDataSource} from "../../infrastructure/db/AppDataSource.js";
+
 export class DeleteSessionUseCase {
-  async execute(sessionId: number, actorId: number): Promise<void> {
-    throw new Error("Not implemented");
+
+  _userRepository : Repository<User>;
+  _sessionRepository : Repository<Session>;
+
+  constructor() {
+    this._userRepository = AppDataSource.getRepository(User);
+    this._sessionRepository = AppDataSource.getRepository(Session);
+  }
+
+  async execute(sessionId: number, actorId: number, systemBypass : boolean = false): Promise<DeleteResult> {
+
+    const actor: User | null = await this._userRepository.findOneBy({ id: actorId });
+    if (!actor)
+      throw Error("User does not exist");
+
+    const session: Session | null = await this._sessionRepository.findOneBy({ id: sessionId });
+    if (!session)
+      throw Error("Session does not exist");
+
+    if(!systemBypass) {
+      if(session.owner != actor)
+        throw Error("User is not the owner of the session");
+
+      return await this._sessionRepository.delete({ id: sessionId });
+    } else
+      return await this._sessionRepository.delete({ id: sessionId });
+
   }
 }
