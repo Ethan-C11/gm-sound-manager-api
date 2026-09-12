de8858325debdaed591b363df243bf50aba0f04c
Ethan-C11 <ethan.collignon.1102@gmail.com>
2026-09-12 14:18:08 +0200

    fix: entity columns

--- src/infrastructure/db/entities/session.entity.ts
+++ src/infrastructure/db/entities/session.entity.ts
@@ -5,19 +5,26 @@ import {User} from "./user.entity.js";
 export class Session {
     @PrimaryGeneratedColumn()
     id: number;
+
     @ManyToOne(() => User, (user) => user.ownedSessions)
     owner: Relation<User>;
+
     @Column()
     sessionName: string;
+
     @Column()
     inviteCode: string;
+
     @CreateDateColumn()
     createdAt: Date;
+
     @ManyToMany(() => User, (user) => user.joinedSessions)
     @JoinTable()
     sessionMembers: Relation<User[]>;
-    @Column()
-    alreadyPlayedAmbianceMusic : number[];
-    @Column()
-    alreadyPlayedZoneMusic : number[];
+
+    @Column({ type: 'int', array: true, default: [] })
+    alreadyPlayedAmbianceMusic: number[];
+
+    @Column({ type: 'int', array: true, default: [] })
+    alreadyPlayedZoneMusic: number[];
 }
